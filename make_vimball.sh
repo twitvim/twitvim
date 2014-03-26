b3b5d41f1fbde41a68496759315b39fe1b1a5342
Morton Fox <mortonfox@gmail.com>
2014-03-26 16:48:57 +0000

    Added make_vimball script. Moved files into proper folder structure.

---
+++ make_vimball.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+version=`awk '/Version:/ { gsub("\r", ""); print $3 }' plugin/twitvim.vim`
+vmbname="twitvim-${version}.vmb"
+vim -e -N -i NONE -u NORC "+set rtp^=." "+/^FILES:/+1,\$MkVimball! ${vmbname}" +quit $0
+exit 0
+
+FILES:
+plugin/twitvim.vim
+doc/twitvim.txt
